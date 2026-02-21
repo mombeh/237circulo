8f56ef5eaee34cfe63740ac4d68e0cb9af6b8ced
mombeh <nadinemombeh@gmail.com>
2026-02-21 19:22:38 +0100

    fix: infinite loading and ESLint issues

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,18 @@
-import nextConfig from 'eslint-config-next'
-
 export default [
-  ...nextConfig,
-]
+  {
+    files: ['**/*.{js,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        browser: true,
+        node: true,
+        es2021: true,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
